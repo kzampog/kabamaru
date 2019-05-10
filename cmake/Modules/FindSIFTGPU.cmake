f92450d7d3250549c6000c147f18efda271fd2ff
Konstantinos Zampogiannis <kzampog@gmail.com>
2019-05-09 21:28:55 -0400

    Updated cmake scripts

--- cmake/Modules/FindSIFTGPU.cmake
+++ cmake/Modules/FindSIFTGPU.cmake
@@ -1,4 +1,4 @@
-find_path(SIFTGPU_INCLUDE_DIRS SiftGPU.h HINTS /usr/local/include)
+find_path(SIFTGPU_INCLUDE_DIRS SiftGPU.h HINTS /usr/local/include/siftgpu)
 find_library(SIFTGPU_LIBRARIES siftgpu HINTS /usr/local/lib)
 if(SIFTGPU_LIBRARIES)
     SET (SIFTGPU_FOUND TRUE)    
